892cdd99ea34a6eb92637d04992e6270797b6f2e
vinayakdubey27 <vinayakdubey827@gmail.com>
2025-06-10 15:32:02 +0530

    Update CurrentAccount.java

--- src/model/CurrentAccount.java
+++ src/model/CurrentAccount.java
@@ -1,4 +1,23 @@
 package model;
 
-public class CurrentAccount {
+public class CurrentAccount extends Account {
+    private static final double OVERDRAFT_LIMIT = -1000;
+
+    public CurrentAccount(int accountId, double initialBalance) {
+        super(accountId, initialBalance);
+    }
+
+    @Override
+    public boolean withdraw(double amount) {
+        if(amount > 0 && (balance - amount) >= OVERDRAFT_LIMIT) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String getAccountType() {
+        return "Current";
+    }
 }
