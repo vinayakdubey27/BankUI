84237a67d3d9ae765455b2b0817d7c1c61a0be3e
vinayakdubey27 <vinayakdubey827@gmail.com>
2025-06-10 15:32:30 +0530

    Update SavingsAccount.java

--- src/model/SavingsAccount.java
+++ src/model/SavingsAccount.java
@@ -1,4 +1,23 @@
 package model;
 
-public class SavingAccount {
+public class SavingsAccount extends Account {
+    private static final double MIN_BALANCE = 500;
+
+    public SavingsAccount(int accountId, double initialBalance) {
+        super(accountId, initialBalance);
+    }
+
+    @Override
+    public boolean withdraw(double amount) {
+        if(amount > 0 && (balance - amount) >= MIN_BALANCE) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String getAccountType() {
+        return "Savings";
+    }
 }
