2023c154e25080047ddf96da9f63d51171ebfd5b
vinayakdubey27 <vinayakdubey827@gmail.com>
2025-06-10 15:31:30 +0530

    Update Account.java

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,4 +1,35 @@
 package model;
 
-public class Account {
+public abstract class Account {
+    protected int accountId;
+    protected double balance;
+
+    public Account(int accountId, double initialBalance) {
+        this.accountId = accountId;
+        this.balance = initialBalance;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if(amount > 0) {
+            balance += amount;
+        }
+    }
+
+    // Withdraw returns true if successful, false otherwise
+    public abstract boolean withdraw(double amount);
+
+    public abstract String getAccountType();
+
+    @Override
+    public String toString() {
+        return accountId + "," + getAccountType() + "," + balance;
+    }
 }
